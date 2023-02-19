b6d352785174e251f97320f349d84de678ef5ce8
Mpablom <100524470+Mpablom@users.noreply.github.com>
2023-02-19 02:58:04 -0300

    cargar tarjetas de index.html dinamicamente desde data.json

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let card = document.getElementById("card")
+const actualYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = actualYear;
+
+function traer(){
+    fetch('data.json').then(response=>response.json()).then(datos=>{
+        // console.log(datos);
+        cards(datos);
+    })
+};
+function cards(datos){
+    card.innerHTML='';
+    for(let valores of datos.events){
+        card.innerHTML += `<div class="col-sm-5 col-md-4 col-lg-4 col-xl-3">
+                                <div class="card">
+                                    <img src="${valores.image}" class="card-img-top m-4" alt="${valores.name}">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${valores.name}</h5>
+                                        <p class="card-text">${valores.description}</p>
+                                    </div>
+                                    <div class="card-footer d-inline-flex border-top-0">
+                                        <p> Price:<span class="pe-2">${valores.price}.-</span></p>
+                                        <a href="./details.html?id=${valores.id}" class="btn btn-dark w-75 seeMore">See More</a>
+                                    </div>  
+                                </div>
+                            </div>`;
+    }
+}
+traer();
