6616f4a66ef37e8d7cb41141dd542b2ded245054
Mpablom <100524470+Mpablom@users.noreply.github.com>
2023-02-21 20:16:04 -0300

    retoques en los archivos js

--- js/events-past.js
+++ js/events-past.js
@@ -1,32 +1,17 @@
-import { actualYear } from "./main.js";
+import {cardService } from "./main.js";
 
 function productosPast(){
-    fetch('./data.json')
-    .then(response=>response.json())
-    .then(datos=>{cardsPast(datos)})
+    cardService.readProducts()
+    .then(datos=>{cardsEventsPast(datos)});
 };
 
-function cardsPast(datos){
+function cardsEventsPast(datos){
     card.innerHTML='';
     for(let valores of datos.events){
         let currentDate = new Date(datos.currentDate);
         let eventDate = new Date(valores.date);
-        console.log(currentDate);
-        console.log(eventDate);
         if (eventDate > currentDate) {
-            card.innerHTML += `<div class="col-ssm-5 col-md-4 col-lg-4 col-xl-3">
-                                    <div class="card">
-                                        <img src="${valores.image}" class="card-img-top m-4" alt="${valores.name}">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${valores.name}</h5>
-                                            <p class="card-text">${valores.description}</p>
-                                        </div>
-                                        <div class="card-footer d-inline-flex border-top-0">
-                                            <p> Price:<span class="pe-2">${valores.price}.-</span></p>
-                                            <a href="./details.html?id=${valores.id}" class="btn btn-dark w-75 seeMore">See More</a>
-                                        </div>  
-                                    </div>
-                                </div>`;
+            cardService.cardsDates(valores);
         }
     }
 };
--- js/events-upcoming.js
+++ js/events-upcoming.js
@@ -1,31 +1,18 @@
-import { actualYear } from "./main.js";
+import {cardService } from "./main.js";
 
 
 function productosUpcoming(){
-        fetch('./data.json')
-        .then(response=>response.json())
-        .then(datos=>{cardsUpcoming(datos)})
+        cardService.readProducts()
+        .then(datos=>{cardsEventsUpcoming(datos)});
 };
 
-function cardsUpcoming(datos){
+function cardsEventsUpcoming(datos){
     card.innerHTML='';
     for(let valores of datos.events){
         let currentDate = new Date(datos.currentDate);
         let eventDate = new Date(valores.date);
         if (eventDate < currentDate) {
-            card.innerHTML += `<div class="col-ssm-5 col-md-4 col-lg-4 col-xl-3">
-                                    <div class="card">
-                                        <img src="${valores.image}" class="card-img-top m-4" alt="${valores.name}">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${valores.name}</h5>
-                                            <p class="card-text">${valores.description}</p>
-                                        </div>
-                                        <div class="card-footer d-inline-flex border-top-0">
-                                            <p> Price:<span class="pe-2">${valores.price}.-</span></p>
-                                            <a href="./details.html?id=${valores.id}" class="btn btn-dark w-75 seeMore">See More</a>
-                                        </div>  
-                                    </div>
-                                </div>`;
+            cardService.cardsDates(valores);
         }
     }
 };
